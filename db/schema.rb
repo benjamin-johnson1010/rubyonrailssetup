4415faa0399c4a9d8960c83ce5480fd65ef2373c
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2017-03-08 00:27:09 -0600

    changes to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307192612) do
+ActiveRecord::Schema.define(version: 20170308061927) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "body"
   end
 
 end
