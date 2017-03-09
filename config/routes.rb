14ffb7cba2ba94932924680239fd432c13ae7378
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2017-03-08 23:42:11 -0600

    can update and delete entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/pages', to: 'pages#index'
-  post '/pages', to: 'pages#create'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  get '/pages/:id', to:'pages#show', as:  'page'
-
-  # post '/pages', to: 'pages#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :pages
+  end
